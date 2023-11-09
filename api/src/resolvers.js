12579b16363a0f2b9f7048278d9cb8ef6b5b727b
bryan trang <bryanltrang@gmail.com>
2023-11-08 22:34:55 -0800

    graphql schema and resolvers

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -5,19 +5,42 @@
 
 module.exports = {
   Query: {
-    
+    getUser(_, { input }, ctx) {
+      return ctx.models.User.findOne(input ? input : {});
+    },
+    getPet(_, { input }, ctx) {
+      console.log('QUERY => pet')
+      return ctx.models.Pet.findOne(input ? input : {});
+    },
+    pets(_, { input }, ctx) {
+      return ctx.models.Pet.findMany(input ? input : {});
+    },
+    users(_,{ input }, ctx) {
+      return ctx.models.User.findMany(input ? input : {});
+    }
   },
   Mutation: {
-    
+    newPet(_, { input }, ctx) {
+      const newPet = ctx.models.Pet.create(input)
+      return newPet
+    }
   },
   Pet: {
     img(pet) {
       return pet.type === 'DOG'
         ? 'https://placedog.net/300/300'
         : 'http://placekitten.com/300/300'
-    }
+    },
+    owner(pet, _, ctx) {
+      console.log('PET => owner')
+      // return ctx.models.User.findOne();
+      return ctx.user
+    },
   },
   User: {
-    
+    pets(user, _, ctx) {
+      console.log('USER => pets')
+      return ctx.models.Pet.findMany()
+    }
   }
 }
--- api/src/schema.js
+++ api/src/schema.js
@@ -4,8 +4,46 @@ const { gql } = require('apollo-server')
  * Type Definitions for our Schema using the SDL.
  */
 const typeDefs = gql`
-  
+  enum PetType {
+    DOG
+    CAT
+  }
 
+  type User {
+    id: ID!
+    username: String!
+    pets: [Pet]
+  }
+
+  type Pet {
+    id: ID!
+    createdAt: String!
+    name: String!
+    type: PetType!
+    img: String!
+    owner: User!
+  }
+
+  input PetInput {
+    name: String
+    type: PetType
+  }
+
+  input NewPetInput {
+    name: String!
+    type: PetType!
+  }
+
+  type Query {
+    getUser: User!
+    users: [User]!
+    getPet(id: ID!): Pet!
+    pets(input: PetInput): [Pet]!
+  }
+
+  type Mutation {
+    newPet(input: NewPetInput): Pet!
+  }
 `;
 
 module.exports = typeDefs
